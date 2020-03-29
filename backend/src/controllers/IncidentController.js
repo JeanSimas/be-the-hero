cfbc9e79a7afaecfa7a52b431403f7a98b598fd7
Jean Simas <jeansimas12@hotmail.com>
2020-03-29 00:36:19 -0300

    Fixing pagination problem

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -20,14 +20,13 @@ module.exports = {
     async index(request, response) {
         const { page = 1 } = request.query
 
-
         try {
             const [count] = await connection('incidents')
                 .count()
             const incidents = await connection('incidents')
                 .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
                 .limit(5)
-                .offset((page - 1 * 5))
+                .offset((page - 1) * 5)
                 .select(['incidents.*', 'ongs.name', 'ongs.email', 'whatsapp', 'ongs.city', 'ongs.uf'])
 
             response.header('X-Total-Count', count['count(*)'])
